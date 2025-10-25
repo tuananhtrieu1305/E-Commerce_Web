dcb61075d93b7d069e908c426369ffa73b6591d4
Vinhdiesel28 <nguyenvinh2149@gmail.com>
2025-10-25 16:25:19 +0700

    Cập nhật .gitignore

--- backend/src/main/java/com/backend/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/backend/backend/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3002")); // FE React
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // FE React
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
